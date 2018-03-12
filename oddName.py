ee4a9f3345fb9dced34d408a2203eb7ea323eb54
jc463595 <isaac.montiadis@my.jcu.edu.au>
2018-03-13 09:52:18 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,8 @@
 """Isaac"""
+while True:
+    name = str(input("What is your name: "))
+
+    if name.alpha():
+        print("Your name is {}".format(name))
+        break
+    else: name = str(input("What is your name: "))
